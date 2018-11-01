9acee31ab6c0ee748c93bb3ad7f20972bc837f73
tonyjin <jintao@bizseer.com>
2018-11-01 17:54:28 +0800

    RM: rm.h rm_rid.h added and filesystem

---
+++ rm_rid.h
@@ -0,0 +1,8 @@
+class RID {
+	public:
+	    RID();                        // Default constructor
+	    ~RID();                        // Destructor
+	    RID(PageNum pageNum, SlotNum slotNum); // Construct RID from page and slot number
+	    RC GetPageNum(PageNum &pageNum) const;  // Return page number
+	    RC GetSlotNum(SlotNum &slotNum) const;  // Return slot number
+};
